77a75b6b5192b571be539321c2fce050ffb48d7c
Stephanie Sison <sjsison27@icloud.com>
2022-09-11 22:25:07 -0700

    laying out the format of App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,16 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+    const [showDogs, setIsDogs] = useState(null)
+
+    useEffect
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default App
